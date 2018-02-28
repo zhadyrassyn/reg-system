57399357e6626381c470b0e0b421b345a5cb340d
Daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-02-28 23:44:54 +0600

    Sign up form with redux form

--- client/src/components/sign_up.js
+++ client/src/components/sign_up.js
@@ -1,25 +1,61 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import {Field, reduxForm} from "redux-form";
+import {connect} from "react-redux"
 
 class SignUp extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label htmlFor={field.id}>{field.label}</label>
+        <input type={field.type} name={field.name} className="form-control" id={field.id} placeholder={field.placeholder} {...field.input}/>
+        {field.hint &&
+          <small id={field.id} className="text-muted">
+            {field.hintText}
+          </small>
+        }
+      </div>
+    )
+  }
+
+  onSubmit(values) {
+    console.log(values)
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
       <div className="container">
-        <form className="mx-auto mt-5 p-4" id="sign_up_form">
-          <div className="form-group">
-            <label htmlFor="email">Email address</label>
-            <input type="email" className="form-control" id="email" placeholder="Enter email"/>
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" className="form-control" id="password" placeholder="Password"/>
-            <small id="password" className="text-muted">
-              Must be 8-20 characters long.
-            </small>
-          </div>
-          <div className="form-group">
-            <label htmlFor="passwordConfirm">Confirm password</label>
-            <input type="password" className="form-control" id="passwordConfirm" placeholder="Confirm password"/>
-          </div>
+        <form className="mx-auto mt-5 p-4" id="sign_up_form" onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+          <Field
+            label="Email address"
+            name="email"
+            id="email"
+            type="email"
+            placeholder="Enter email"
+            component={this.renderField}
+          />
+
+          <Field
+            label="Password"
+            name="password"
+            id="password"
+            type="password"
+            placeholder="Password"
+            hint="true"
+            hintText="Must be 8-20 characters long."
+            component={this.renderField}
+          />
+
+          <Field
+            label="Confirm password"
+            name="confirmPasword"
+            id="confirmPasword"
+            type="password"
+            placeholder="Confirm password"
+            component={this.renderField}
+          />
+
           <button type="submit" className="btn btn-primary" id="sign_up_btn">Submit</button>
         </form>
       </div>
@@ -27,4 +63,6 @@ class SignUp extends Component {
   }
 }
 
-export default SignUp
+export default reduxForm({
+  form: "SignUpForm"
+})(connect(null, null)(SignUp))
