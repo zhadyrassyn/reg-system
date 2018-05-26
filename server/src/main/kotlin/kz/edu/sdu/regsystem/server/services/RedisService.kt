c70e5a8b16a1e25badf75a185495f61b005d4830
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-05-26 15:18:47 +0600

    Redis. get and set methods set up through json

--- server/src/main/kotlin/kz/edu/sdu/regsystem/server/services/RedisService.kt
+++ server/src/main/kotlin/kz/edu/sdu/regsystem/server/services/RedisService.kt
@@ -1,9 +1,72 @@
 package kz.edu.sdu.regsystem.server.services
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kz.edu.sdu.regsystem.server.domain.*
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Service
+import java.util.concurrent.TimeUnit
 
 @Service
 class RedisService(val redisTemplate: RedisTemplate<String, String>) {
+    fun set(key: String, list: ArrayList<*>) {
+        val gson = Gson()
+        val json = gson.toJson(list)
+        redisTemplate.opsForValue().set(key, json, 30, TimeUnit.DAYS)
+    }
 
+    fun getAreas(key: String) : List<Area> {
+        val data = redisTemplate.opsForValue().get(key)
+        val gson = Gson()
+        val collectionType = object : TypeToken<List<Area>>() {
+
+        }.type
+        val areas : List<Area> = gson.fromJson(data, collectionType)
+
+        return areas
+    }
+
+    fun getCities(key: String) : List<City> {
+        val data = redisTemplate.opsForValue().get(key)
+        val gson = Gson()
+        val collectionType = object : TypeToken<List<City>>() {
+
+        }.type
+        val cities : List<City> = gson.fromJson(data, collectionType)
+
+        return cities
+    }
+
+    fun getSchools(key: String) : List<School> {
+        val data = redisTemplate.opsForValue().get(key)
+        val gson = Gson()
+        val collectionType = object : TypeToken<List<School>>() {
+
+        }.type
+        val schools : List<School> = gson.fromJson(data, collectionType)
+
+        return schools
+    }
+
+    fun getFaculties(key: String) : List<Faculty> {
+        val data = redisTemplate.opsForValue().get(key)
+        val gson = Gson()
+        val collectionType = object : TypeToken<List<Faculty>>() {
+
+        }.type
+        val faculties : List<Faculty> = gson.fromJson(data, collectionType)
+
+        return faculties
+    }
+
+    fun getSpecialties(key: String) : List<Specialty> {
+        val data = redisTemplate.opsForValue().get(key)
+        val gson = Gson()
+        val collectionType = object : TypeToken<List<Specialty>>() {
+
+        }.type
+        val specialties : List<Specialty> = gson.fromJson(data, collectionType)
+
+        return specialties
+    }
 }
